008fff768a66b2e34c16740192b072546121b8e1
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-14 20:35:11 +0700

    Первое задание из ДЗ

---
+++ Модуль 1/База/Homework/Numb 1.py
@@ -0,0 +1,4 @@
+first_animal=str(input("Введите первое животное: "))
+second_animal=str(input("Введите второе животное: "))
+print(first_animal,"спит,", second_animal,"идёт")
+print("{} спит, {} идёт".format(first_animal, second_animal))
