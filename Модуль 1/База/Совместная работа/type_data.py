357675626e4c1b8561ad69a514c48afeea7d0f7c
OtdelAnaliticiAlterra <analitic.alterra@gmail.com>
2023-08-14 20:18:17 +0700

    ЗАкончен первый блок с базой

--- Модуль 1/База/Совместная работа/type_data.py
+++ Модуль 1/База/Совместная работа/type_data.py
@@ -1,3 +1,38 @@
+
 var_str = "любая строка"
+var_str3 = 'любая строка'
+var_str3 = '''любая строка dsfdsfsd 
+dsfsdfsdf
+ dsfsdfsdf
+ dsfsdfsdf
+ sdfsdf
+ '''
 var_int = 10
 var_float = 10.5
+my_tumple = (1, 2, 3, 5)
+my_tumple3= tuple([2, 3, 5, 7])
+print(my_tumple)
+print(my_tumple3)
+my_list = [1, 2, 3, 5]
+print(tuple(my_list))
+print(type(var_str3))
+#
+# my_list[0] = 5
+# print(my_list)
+# my_tumple[0] = 5
+# print(my_tumple)
+#
+# my_dict = {
+#     "Ключ": "Значсение",
+#     "Ключ2": 213,
+#     "Клюx3": {
+#         "Товар1": 456,
+#         "Товар3": 234,
+#     },
+#     "ключ4": dict(Вид_цены="Прайс1", Вид_цены1="Прайс3")
+# }
+# print(my_dict )
+
+
+
+
---
+++ Модуль 1/База/Совместная работа/Итерации.py
@@ -0,0 +1,35 @@
+x1 = 1
+x2 = 2
+x3 = 3
+# my_list = ["фыв", "None", "x3"]
+#
+# for elem in my_list:
+#     print(elem)
+#     if elem is None:
+#         break
+# else:
+#     print("".join(my_list))
+# print("Конец")
+# my_dict = {
+#     "Name": "Саша",
+#     "Age": 28,
+#     "City": "Барнаул"
+# }
+#
+# for elem in my_dict:
+#     print(elem, my_dict[elem])
+#
+# print()
+# for key, value in my_dict.items():
+#     print(key, value)
+#
+# print()
+# for value in my_dict.values():
+#     print(value)
+#
+# print(type(my_dict.values()))
+# print(list(my_dict.values())[1])
+text = "Поздравляем с победой, {0} {0} {0}!".format("Григорий")
+text = "Поздравляем с победой, {name} {addword} {fgh}!".format(name="Семен", addword="ывфыв", fgh=123)
+
+print(text)
