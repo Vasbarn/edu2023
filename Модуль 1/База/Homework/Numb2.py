269ba4683cf29ecfb1887ce14cf4c7f01fab56e7
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-14 20:43:34 +0700

    Первое задание из ДЗ

---
+++ Модуль 1/База/Homework/Numb2.py
@@ -0,0 +1,4 @@
+departure_city = input("Город вылета: ")
+arrive_city = input("Город прилета: ")
+print("Город вылета:{city1}-Город прилета:{city2}".format(city1=departure_city,city2=arrive_city))
+print(f"Город вылета:{departure_city}-Город прилета:{arrive_city}")
