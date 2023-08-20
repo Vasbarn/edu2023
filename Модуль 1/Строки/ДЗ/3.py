0a61840a3aa403537cb45553857d422010d9a717
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-20 15:09:41 +0700

    4 номер

---
+++ Модуль 1/Строки/ДЗ/3.py
@@ -0,0 +1,5 @@
+filename=input("Введите название файла: ")
+starter=('@', '№', '$', '%', '^', '&', '*', '()')
+end=(".txt",'.docx')
+if filename.startswith(starter)==False and filename.endswith(end)==True:
+    print(filename)
