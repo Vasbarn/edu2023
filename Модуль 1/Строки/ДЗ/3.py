fb6778f98fda3904f7dcd11de671788a779f0ada
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-21 16:32:56 +0700

    7 задание из дз

--- Модуль 1/Строки/ДЗ/3.py
+++
@@ -1,5 +0,0 @@
-filename = input("Введите название файла: ")
-starter = ('@', '№', '$', '%', '^', '&', '*', '()')
-end = (".txt",'.docx')
-if filename.startswith(starter)==False and filename.endswith(end)==True:
-    print(filename)
