4cd4a0ec9fcfc24daef923d04ec7d2a3d06e9875
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-21 17:37:23 +0700

    3

--- Модуль 1/Списки/Дз/8.py
+++ Модуль 1/Списки/Дз/8.py
@@ -1,6 +1,2 @@
-N = int(input("Введите количество элементов списка: "))
-list = []
-for i in range(N):
-    count = int(input("Введите число: "))
-    list.append(count)
+list = [i for i in input("Введите список: ").split()]
 print(sorted(list))
--- Модуль 1/Строки/Совместная работа.py
+++ Модуль 1/Строки/Совместная работа.py
@@ -1,13 +1,15 @@
-my_sting = "ЛРЫЛОВРАФЫЛРАЛО-01.02.2023"
+# my_sting = "ЛРЫЛОВРАФЫЛРАЛО-01.02.2023"
+#
+# my_list = my_sting.split("-")
+# # if len(my_list) == 3:
+# #     print("Correct")
+# # else:
+# #     print("Incorrect")
+# print(my_list)
+# print(my_list[-1])
+# print(my_sting.find("О"))
+# print(my_sting.count("О"))
+# print("тест" in "Тестирование".lower())
+# print(my_sting[-10:])
 
-my_list = my_sting.split("-")
-# if len(my_list) == 3:
-#     print("Correct")
-# else:
-#     print("Incorrect")
-print(my_list)
-print(my_list[-1])
-print(my_sting.find("О"))
-print(my_sting.count("О"))
-print("тест" in "Тестирование".lower())
-print(my_sting[-10:])
+s = "привет мир ghjthgsdasdsas".split()
