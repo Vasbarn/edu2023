9b73d759fb1ee16eb8cc85e0b2d9810e066921b7
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-22 03:43:38 +0700

    6 задание из дз

---
+++ Модуль 1/if условия/Дз/6.py
@@ -0,0 +1,7 @@
+cube_1 = int(input("Выпавшее число: "))
+cube_2 = int(input("Выпавшее число: "))
+if cube_1 >= cube_2:
+    print(f"Разность: {cube_1-cube_2} \n Игрок платит")
+else:
+    print(f"Сумма: {cube_1 + cube_2} \n Владелец платит")
+
