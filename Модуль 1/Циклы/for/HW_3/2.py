bd2037d7481969b967e17db84aaefd2dc0287a7d
Bogdan096 <potapovbogdan@yandex.ru>
2023-08-24 20:40:52 +0700

    2 задание из дз

---
+++ Модуль 1/Циклы/for/HW_3/2.py
@@ -0,0 +1,10 @@
+# text = input("Введите текст: ")
+# for idx, symbol  in enumerate(text):
+#     if (symbol) == "*":
+#         break
+# print(f"Звездочка находится на {idx} месте")
+
+text = input("Введите текст: ")
+print(f"Звездочка стоит на {text.find('*')} месте")
+
+
