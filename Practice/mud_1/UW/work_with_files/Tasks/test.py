b3862f03451dcc937223fe139a2f92972426bb43
Vasbarn <vasilievbarnaul@mail.ru>
2023-09-11 19:13:04 +0700

    11 09 2023

---
+++ Practice/mud_1/UW/work_with_files/Tasks/test.py
@@ -0,0 +1,40 @@
+# if True:
+# 	print("Блок True")
+# else:
+# 	print("Блок False")
+#
+# if 1:
+# 	print("Блок True")
+# else:
+# 	print("Блок False")
+#
+# 1 == True
+#
+# 0 == False
+#
+import random
+
+result = random.randint(0, 1)
+if result:
+	print("Блок True, значение", result)
+else:
+	print("Блок False, значение", result)
+
+result = random.randint(0, 1)
+if result:
+	print("Блок True, значение", result)
+else:
+	print("Блок False, значение", result)
+
+result = random.randint(0, 1)
+if result:
+	print("Блок True, значение", result)
+else:
+	print("Блок False, значение", result)
+
+result = random.randint(0, 1)
+if result:
+	print("Блок True, значение", result)
+else:
+	print("Блок False, значение", result)
+
