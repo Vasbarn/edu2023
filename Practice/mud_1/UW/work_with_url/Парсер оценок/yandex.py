e4faf470aa62fddf5c68fcddcd708f85c4e06be3
Bogdan096 <potapovbogdan@yandex.ru>
2023-10-23 01:01:03 +0700

    4 номер

--- Practice/mud_1/UW/work_with_url/Парсер оценок/yandex.py
+++ Practice/mud_1/UW/work_with_url/Парсер оценок/yandex.py
@@ -15,12 +15,15 @@ def get_links(way: str) -> Dict[str, str]:
 def get_rating(url: str, adres: str, listing: list) -> list:
     response = requests.get(url, headers=header).text
     soup = BeautifulSoup(response, "lxml")
-    main = soup.find(class_ = "_1tfwnxl")
-    contains = main.find(class_="_y10azs")
-    contains2 = main.find(class_="_jspzdm")
+    main = soup.find(class_ = "business-header-rating-view__user-rating")
+
+    rate = main.find(class_ = "business-rating-badge-view__rating-text _size_m")
+    calls = main.find(class_ = "business-header-rating-view__text _clickable")
+    print(rate)
+    print(calls)
     diction = {}
-    diction["Количество отзывов"] = contains2.text
-    diction["Рейтинг"] = contains.text
+    diction["Количество отзывов"] = calls.text
+    diction["Рейтинг"] = rate.text
     diction["Название магазина"] = adres
     listing.append(diction)
 
@@ -29,15 +32,13 @@ def get_rating(url: str, adres: str, listing: list) -> list:
 
 
 spisok = []
-path = os.path.abspath("Список магазинов.xlsx")
+path = os.path.abspath("Магазины Яндекс.xlsx")
 links: dict = get_links(path).get("Ссылка")
 for key,value in links.items():
-    get_rating(value, key, spisok)
+    get_rating(value,path,spisok)
 
-dataf = pandas.DataFrame(spisok)
-otkritie = pandas.read_excel(path, sheet_name = "Лист1")
-df = otkritie.merge(dataf, left_on="Название магазина", right_on="Название магазина")
-df.to_excel(path, sheet_name= "Лист1", index=False)
-print(df)
-
-
+# dataf = pandas.DataFrame(spisok)
+# otkritie = pandas.read_excel(path, sheet_name = "Лист1")
+# df = otkritie.merge(dataf, left_on="Название магазина", right_on="Название магазина")
+# df.to_excel(path, sheet_name= "Лист2", index=False)
+# print(df)
