d1f5b313ab97619443406a00753e4b138c0030c8
Bogdan096 <potapovbogdan@yandex.ru>
2024-03-06 17:26:05 +0700

    Pflfyb  из дз

--- Curse/Mongo.py
+++ Curse/Mongo.py
@@ -12,9 +12,11 @@ ids = []
 naming = []
 trd_period = []
 trd_volume = []
-trd_helper = []
 bal_period = []
 bal_value = []
+loyal_period = []
+loyal_value = []
+loyal_data = {}
 dt ={}
 balance_data = {}
 trd_data = {}
@@ -33,39 +35,62 @@ for i in range(len(naming)):
     dt[naming[i]]=ids[i]
 
 tbl_list = ["DT_NSO_1400_003V1", "DT_NSO_0800_008V1"]
-url = "https://opendata.1212.mn/api/Data?type=xml"
+url = "https://opendata.1212.mn/api/Data?type=json"
 datas = {
     "tbl_id": "DT_NSO_1400_003V1",
 }
-
+datas1 = {
+    "tbl_id": "DT_NSO_0800_008V1",
+}
+datas2 = {
+    "tbl_id": "DT_NSO_0700_010V1",
+    # "Period": ["201506", "201505"]
+}
 resp = requests.post(url, json=datas)
 soup = BeautifulSoup(resp.text, "lxml")
 
+# print(soup)
+resp1 = requests.post(url, json=datas1)
+soup1 = BeautifulSoup(resp1.text, "lxml")
 
+resp2 = requests.post(url, json=datas2)
+soup2 = BeautifulSoup(resp2.text, "lxml").text
+print(soup2.split("Total liabilities"))
 
-periods = soup.findAll("period")
-for elem in periods:
-    trd_period.append(elem.text)
-values = soup.findAll("dtval_co")
-for elem in values:
-    trd_volume.append(elem.text)
-help = soup.findAll("scr_eng")
-for elem in help:
-    trd_helper.append(elem.text)
-for i in range(0, len(trd_period), 4):
-    trd_data[trd_period[i]] = trd_volume[i]
-all_data["Внешнеторговый оборот"] = trd_data
-periods = soup.findAll("period")
-for elem in periods:
-    bal_period.append(elem.text)
-values = soup.findAll("dtval_co")
-for elem in values:
-    bal_value.append(elem.text)
-for i in range(len(bal_period)):
-    balance_data[bal_period[i]] = bal_value[i]
-all_data["Баланс бюджета общего управления"] = balance_data
-df = pd.DataFrame(all_data)
-print(df)
+#
+# periods = soup.findAll("period")
+# for elem in periods:
+#     trd_period.append(elem.text)
+# values = soup.findAll("dtval_co")
+# for elem in values:
+#     trd_volume.append(elem.text)
+# for i in range(0, len(trd_period), 4):
+#     trd_data[trd_period[i]] = trd_volume[i]
+# all_data["Внешнеторговый оборот"] = trd_data
+#
+# periods = soup1.findAll("period")
+# for elem in periods:
+#     bal_period.append(elem.text)
+# values = soup1.findAll("dtval_co")
+# for elem in values:
+#     bal_value.append(elem.text)
+# for i in range(len(bal_period)):
+#     balance_data[bal_period[i]] = bal_value[i]
+# all_data["Баланс бюджета общего управления"] = balance_data
+#
+# periods = soup2.findAll("period")
+# for elem in periods:
+#     loyal_period.append(elem.text)
+# values = soup2.findAll("dtval_co")
+# for elem in values:
+#     loyal_value.append(elem.text)
+# for i in range(11, len(loyal_period), 10):
+#     # loyal_data[loyal_period[i]] = loyal_value[i]
+#     print(loyal_value[i])
+# print(loyal_data)
+# all_data["Общие обязательства"] = loyal_data
+# df = pd.DataFrame(all_data)
+# print(df)
 
 
 
--- alterra/znak/Class2.py
+++ alterra/znak/Class2.py
@@ -10,7 +10,7 @@ urllib3.disable_warnings(category=InsecureRequestWarning)
 from multiprocessing import Pool
 import sys
 import time
-
+import itertools
 sys.setrecursionlimit(10000)
 
 
@@ -19,11 +19,13 @@ class Znak():
        self.datas = dict()
        self.articles = list()
        self.types = list()
+       self.types_ga = []
        self.items = []
+       self.items_ga = []
+       self.datas_ga = []
     def get_page_data_brn(self, href, page):
         data = self.datas
         start = time.time()
-        # link = "https://znakooo.ru/catalog/dveri/" + f"?PAGEN_4={page}"
         link = "https://znakooo.ru" + href + f"?PAGEN_4={page}"
         response = requests.get(link)
         soup = BeautifulSoup(response.text, "lxml")
@@ -55,11 +57,54 @@ class Znak():
             for art in article:
                 a = art.find_all("span")
                 artings = a[-1:]
-                for val in artings:
-                    self.articles.append(val.text)
-            for arts, typing in self.articles, self.types:
-                data[arts] = (last_link,name,price,cardprice, typing )
+                #     self.articles.append(val.text)
+                for val,typing in itertools.product(artings, self.types):
+                    data[val.text] = (last_link,name,price,cardprice,typing)
+        print(data)
         return data
+
+
+
+    def get_page_data_ga(self, href, page):
+        data_ga = self.datas_ga
+        start = time.time()
+        link = "http://galtaysk.znakooo.ru" + href + f"?PAGEN_4={page}"
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "lxml")
+        old_pr = soup.find_all(class_="name p-product__title")
+        for op in old_pr:
+            hrf = op.find("a").get("href")
+            last_link = "https://znakooo.ru" + hrf
+
+            try:
+                response = requests.get(last_link)
+                soup = BeautifulSoup(response.text, 'lxml')
+                name = soup.find(class_="product-title").text
+            except(requests.exceptions.ConnectTimeout, AttributeError, PermissionError):
+                print("r", end="")
+                continue
+
+            cardprice = soup.find(class_="p-price p-price_strong").text.strip()
+
+            type = soup.find(class_="p-price").text.strip()
+            price = type.split("руб.")[0]
+
+            if "шт" in type:
+                self.types_ga.append("Цена ЗнакБарнаул за шт")
+            elif "Уп" in type:
+                self.types_ga.append("Цена ЗнакБарнаул за Уп")
+            elif "пог. м" in type:
+                self.types_ga.append("Цена ЗнакБарнаул за пог. м")
+            article = soup.find_all(class_="product-characteristics")
+            for art in article:
+                a = art.find_all("span")
+                artings = a[-1:]
+                #     self.articles.append(val.text)
+                for val,typing in itertools.product(artings, self.types_ga):
+                    data_ga[val.text] = (last_link,name,price,cardprice,typing)
+
+        return data_ga
+
 
     def gather_data_brn(self):
         url = 'http://znakooo.ru/catalog/'
@@ -80,67 +125,99 @@ class Znak():
             for i in range(1, int(max_page) + 1):
                 self.items.append((group_href, i))
 
-    # def gather_data_ga():
-    #     url = 'http://galtaysk.znakooo.ru/catalog/'
-    #     response = requests.get(url)
-    #     soup = BeautifulSoup(response.text, 'lxml')
-    #     btn = soup.find_all(class_="collection")
-    #     for group in btn:
-    #
-    #         group_href = group.find("a").get("href").strip()
-    #
-    #         x = url[:-9] + group_href
-    #         respon = requests.get(x)
-    #         soup = BeautifulSoup(respon.text, 'lxml')
-    #
-    #         mxpgs = soup.find("div", class_="products__list products__list_e1").get("data-showmore").split(",")
-    #
-    #         max_page = mxpgs[0]
-    #         for i in range(1, int(max_page) + 1):
-    #             items_ga.append((group_href, i))
+    def gather_data_ga(self):
+        url = 'http://galtaysk.znakooo.ru/catalog/'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        btn = soup.find_all(class_="collection")
+        for group in btn:
+
+            group_href = group.find("a").get("href").strip()
+
+            x = url[:-9] + group_href
+            respon = requests.get(x)
+            soup = BeautifulSoup(respon.text, 'lxml')
+
+            mxpgs = soup.find("div", class_="products__list products__list_e1").get("data-showmore").split(",")
+
+            max_page = mxpgs[0]
+            for i in range(1, int(max_page) + 1):
+                self.items_ga.append((group_href, i))
     #
 
-    def main(self, x):
+    def main(self, x, y):
+        keys = []
+        names = []
+        price = []
+        cprice = []
+        link = []
+        type = []
+        keys_ga = []
+        names_ga = []
+        price_ga = []
+        cprice_ga = []
+        link_ga = []
+        type_ga = []
+        for data_brn in x:
+            for key, item in data_brn.items():
+                keys.append(key)
+                names.append(item[1])
+                price.append(item[2])
+                cprice.append(item[3])
+                link.append(item[0])
+                type.append(item[4])
+        for data_ga in y:
+            for key, item in data_ga.items():
+                keys_ga.append(key)
+                names_ga.append(item[1])
+                price_ga.append(item[2])
+                cprice_ga.append(item[3])
+                link_ga.append(item[0])
+                type_ga.append(item[4])
 
 
+        new_slovar = {
+            "Код": 1,
+            "Конкуренты": "Знак Майма",
+            "Артикул": keys,
+            "Наименование": names,
+            "Вид цены": type,
+            "Цена": price,
+            "Цена по карте": cprice,
+            "Ссылка": link
+        }
+        new_slovar_ga = {
+            "Код": 1,
+            "Конкуренты": "Знак Майма",
+            "Артикул": keys_ga,
+            "Наименование": names_ga,
+            "Вид цены": type_ga,
+            "Цена": price_ga,
+            "Ссылка": link_ga
+        }
 
-        dannye = dict()
-        for elem in x:
-            for key, item in elem.items():
-                dannye[key] = item
-        print(dannye)
-        # new_slovar_ga = {
-        #     "Код": 1,
-        #     "Конкуренты": "Знак Майма",
-        #     "Артикул": articles_ga,
-        #     "Наименование": names_ga,
-        #     "Вид цены": types_ga,
-        #     "Цена": prices_ga,
-        #     "Ссылка": linkages_ga
-        # }
-
-        # df = pd.DataFrame(new_slovar)
-        # # df2 = pd.DataFrame(new_slovar_ga)
-        # path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
-        # if os.path.exists(path):
-        #     flag = open(path, "r")
-        #     flag.close()
-        #     os.remove(path)
-        #     path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
-        #     writer = pd.ExcelWriter("Выгрузка цен Знак1.xlsx", engine="xlsxwriter")
-        #     df.to_excel(writer,
-        #                 sheet_name="Барнаул",
-        #                 index=False)
-        #     # df2.to_excel(writer,
-        #     #             sheet_name="Майма",
-        #     #             index=False)
-        # else:
-        #     with ExcelWriter(path, mode="w") as writer:
-        #         df.to_excel(writer, sheet_name="Барнаул")
-        #         # df2.to_excel(writer, sheet_name="Майма")
-        # path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
-        # end = time.time()
-        # print(end - start)
+        df = pd.DataFrame(new_slovar)
+        df2 = pd.DataFrame(new_slovar_ga)
+        path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
+        if os.path.exists(path):
+            flag = open(path, "r")
+            flag.close()
+            os.remove(path)
+            path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
+            writer = pd.ExcelWriter("Выгрузка цен Знак1.xlsx", engine="xlsxwriter")
+            df.to_excel(writer,
+                        sheet_name="Барнаул",
+                        index=False)
+            df2.to_excel(writer,
+                        sheet_name="Майма",
+                        index=False)
+        else:
+            with ExcelWriter(path, mode="w") as writer:
+                df.to_excel(writer, sheet_name="Барнаул")
+                df2.to_excel(writer, sheet_name="Майма")
+        path = os.path.abspath("../Выгрузка цен Знак1.xlsx")
+        end = time.time()
+        print(end - start)
 
 
 if __name__ == "__main__":
@@ -148,12 +225,12 @@ if __name__ == "__main__":
     start = time.time()
 
     znk.gather_data_brn()
-    # gather_data_ga()
+    znk.gather_data_ga()
 
     pool = Pool(processes=60)
     list_data = pool.starmap(znk.get_page_data_brn, znk.items)
-    znk.main(list_data)
-    # pool.starmap(get_page_data_ga,items_ga)
+    list_data_ga = pool.starmap(znk.get_page_data_ga, znk.items)
+    znk.main(list_data,list_data_ga)
     end = time.time()
     print(end - start)
 
