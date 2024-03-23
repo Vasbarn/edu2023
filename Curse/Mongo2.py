68a7ad37d1a1f17b92a2734a15f9fb4f2ffa9cef
Bogdan096 <potapovbogdan@yandex.ru>
2024-03-24 01:30:46 +0700

    классы

--- Curse/Mongo2.py
+++ Curse/Mongo2.py
@@ -10,46 +10,41 @@ import matplotlib.dates as mdates
 
 ids = []
 naming = []
-trd_period = []
-trd_volume = []
-bal_period = []
-bal_value = []
-loyal_period = []
-loyal_value = []
-loyal_data = {}
-dt ={}
+ws_regions = []
+water_val = []
+water_obj = []
+unique_rg = []
 balance_data = {}
 trd_data = {}
 all_data = {}
 
-tbl_list = ["DT_NSO_0300_027V1", "DT_NSO_0800_008V1"]
+tbl_list = ["DT_NSO_0300_027V1", "DT_NSO_1700_002V1"]
 url = "https://opendata.1212.mn/api/Data?type=xml"
 url2 = "https://opendata.1212.mn/api/Data?type=json"
 
 datas = {
-    "tbl_id": "DT_NSO_1700_002V1",
-    "Period": ["2019"]
+    "tbl_id": "DT_NSO_0700_021V1" ,
+    "Period": ["2010"]
 }
 
 resp = requests.post(url, json=datas)
 soup = BeautifulSoup(resp.text, "lxml")
-print(soup)
-
+# print(soup)
 regions = soup.findAll("scr_eng1")
-
-# for elem in regions:
-#     trd_period.append(elem.text)
-# print(trd_period)
-#
+for elem in regions:
+    ws_regions.append(elem.text)
+print(ws_regions)
+# # print(list(set(trd_period)))
+# [unique_rg.append(x) for x in ws_regions if x not in unique_rg]
+# print(unique_rg)
 # div = soup.findAll("scr_eng")
-#
 # for elem in div:
-#     trd_volume.append(elem.text)
+#     water_obj.append(elem.text)
+# print(set(water_obj))
+# values = soup.findAll("dtval_co")
+# for elem in values:
+#     water_val.append(elem.text)
 # print(trd_volume)
-values = soup.findAll("dtval_co")
-for elem in values:
-    trd_volume.append(elem.text)
-print(trd_volume)
 # for i in range(0, len(trd_period), 4):
 #     trd_data[trd_period[i]] = trd_volume[i]
 # all_data["Внешнеторговый оборот"] = trd_data
